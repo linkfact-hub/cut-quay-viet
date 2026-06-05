4e84116fcc849858dc36eb810e27d3fcf7b9f66d
Phan Anh Tuan <pat.0939944931@gmail.com>
2026-06-05 22:42:02 +0700

    Sửa lỗi màn hình trắng, cập nhật base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/cut-quay-viet/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
